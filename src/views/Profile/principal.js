382a976e91974d4b3769da732c0547d002b7ad71
FELIPE MARTINEZ <fimartinez2@uc.cl>
2021-08-06 15:13:21 -0400

    router fix

--- src/views/Profile/principal.js
+++ src/views/Profile/principal.js
@@ -4,7 +4,7 @@ import HeaderApp from "../../components/common/headerApp";
 import { getUserData } from "./components/requests.js";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Layout, Menu, Card, Popconfirm, message } from "antd";
-import { logOut, logOutCesfam } from "../../components/requests"
+import { logOut, logOutCesfam } from "../../components/requests";
 import {
   UserOutlined,
   LogoutOutlined,
@@ -13,11 +13,11 @@ import {
   TeamOutlined,
   PlusCircleOutlined,
   FormOutlined,
-  HeartOutlined
+  HeartOutlined,
 } from "@ant-design/icons";
-import Profile from "../Profile/profile"
-import PatientsVolunteer from "../Profile/patientsVolunteer"
-import NewHelp from "../HelpBoard/components/newHelp"
+import Profile from "../Profile/profile";
+import PatientsVolunteer from "../Profile/patientsVolunteer";
+import NewHelp from "../HelpBoard/components/newHelp";
 import HelpBoard from "../HelpBoard/helpBoard";
 import NewActivity from "./components/newActivity";
 import Report from "./components/report";
@@ -44,7 +44,11 @@ function Principal(def) {
   }, []);
 
   let history = useHistory();
-  if (Object.keys(userData).length !== 0 && userData.status == 1 && localStorage.getItem("userRole") !== "caregivers") {
+  if (
+    Object.keys(userData).length !== 0 &&
+    userData.status == 1 &&
+    localStorage.getItem("userRole") !== "caregivers"
+  ) {
     history.push("/complete-register");
   }
   // colocar la cantidad de actividades realizada si es voluntario | puntuación
@@ -63,37 +67,39 @@ function Principal(def) {
       textAlign: "center",
       lineHeight: "2rem",
       marginTop: "-15px",
-      fontStyle: "italic"
+      fontStyle: "italic",
     },
   };
 
   const user = localStorage.getItem("userRole");
 
-  const text = '¿Estás seguro que quieres cerrar sesión?';
+  const text = "¿Estás seguro que quieres cerrar sesión?";
 
   const handleLogOut = async () => {
-
-    if (user != 'cesfamCoordinators') {
+    if (user != "cesfamCoordinators") {
       await logOut();
-    }
-    else {
+    } else {
       await logOutCesfam();
     }
   };
 
   async function confirm() {
-    message.info('Se ha cerrado sesión');
+    message.info("Se ha cerrado sesión");
     await handleLogOut();
     history.push("/");
   }
 
   return (
     <div>
-      <Router>
+      <Router basename="/rafa-pages-test">
         <HeaderApp />
-        <Layout >
+        <Layout>
           <Layout style={{ height: "100%" }}>
-            <Sider className="mobileHidden" width={211} style={{ height: "102%" }}>
+            <Sider
+              className="mobileHidden"
+              width={211}
+              style={{ height: "102%" }}
+            >
               <Menu
                 mode="inline"
                 defaultSelectedKeys={[`${def.def}`]}
@@ -106,82 +112,135 @@ function Principal(def) {
                   borderWidth: "10px",
                 }}
               >
-
-                {user != 'cesfamCoordinators' &&
+                {user != "cesfamCoordinators" && (
                   <div>
-                    <h2 style={mystyle.title}>{`${userData.firstName} ${userData.lastName}`}</h2>
+                    <h2
+                      style={mystyle.title}
+                    >{`${userData.firstName} ${userData.lastName}`}</h2>
                     <p style={mystyle.rol}>{`${userData.role}`}</p>
                   </div>
-                }
-                {user === 'cesfamCoordinators' &&
+                )}
+                {user === "cesfamCoordinators" && (
                   <div>
-                    <h1 className="text-profile-cesfam"
+                    <h1
+                      className="text-profile-cesfam"
                       style={mystyle.title}
                     >{`${userData.name}`}</h1>
                     <p style={mystyle.rol}>Coordinador CESFAM</p>
                   </div>
-                }
-                {user !== 'cesfamCoordinators' &&
-                  <Menu.Item key="1" icon={<UserOutlined style={{ color: '#51379c' }} />}>
+                )}
+                {user !== "cesfamCoordinators" && (
+                  <Menu.Item
+                    key="1"
+                    icon={<UserOutlined style={{ color: "#51379c" }} />}
+                  >
                     <span className="menuText">Mi perfil</span>
                     <Link to="/profile" />
-                  </Menu.Item>}
+                  </Menu.Item>
+                )}
 
-                {user !== 'patients' && user !== 'caregivers' && user !== 'cesfamCoordinators' &&
-                  <Menu.Item key="2" icon={<SmileOutlined style={{ color: '#51379c' }} />}>
-                    <span className="menuText">Tablero de Ayuda</span>
-                    <Link to="/help-board" />
-                  </Menu.Item>}
+                {user !== "patients" &&
+                  user !== "caregivers" &&
+                  user !== "cesfamCoordinators" && (
+                    <Menu.Item
+                      key="2"
+                      icon={<SmileOutlined style={{ color: "#51379c" }} />}
+                    >
+                      <span className="menuText">Tablero de Ayuda</span>
+                      <Link to="/help-board" />
+                    </Menu.Item>
+                  )}
 
-                {(user === 'patients' || user === 'volunteers' || user === 'caregivers') &&
-                  <Menu.Item key="3" icon={<SmileOutlined style={{ color: '#51379c' }} />}>
+                {(user === "patients" ||
+                  user === "volunteers" ||
+                  user === "caregivers") && (
+                  <Menu.Item
+                    key="3"
+                    icon={<SmileOutlined style={{ color: "#51379c" }} />}
+                  >
                     <a href="/activities">Actividades</a>
-                  </Menu.Item>}
+                  </Menu.Item>
+                )}
 
-                {user === 'coordinators' &&
-                  <Menu.Item key="4" icon={<HeartOutlined style={{ color: '#51379c' }} />}>
+                {user === "coordinators" && (
+                  <Menu.Item
+                    key="4"
+                    icon={<HeartOutlined style={{ color: "#51379c" }} />}
+                  >
                     <a href="/volunteers-coordinators">Voluntarios</a>
-                  </Menu.Item>}
+                  </Menu.Item>
+                )}
 
-                {user === 'coordinators' &&
-                  <Menu.Item key="13" icon={<HeartOutlined style={{ color: '#51379c' }} />}>
+                {user === "coordinators" && (
+                  <Menu.Item
+                    key="13"
+                    icon={<HeartOutlined style={{ color: "#51379c" }} />}
+                  >
                     <a href="/patients-coordinators">Pacientes</a>
-                  </Menu.Item>}
+                  </Menu.Item>
+                )}
 
-                {user === 'coordinators' &&
-                  <Menu.Item key="5" icon={<UsergroupAddOutlined style={{ color: '#51379c' }} />}>
+                {user === "coordinators" && (
+                  <Menu.Item
+                    key="5"
+                    icon={<UsergroupAddOutlined style={{ color: "#51379c" }} />}
+                  >
                     <a href="/volunteer-requests">Solicitudes Voluntario</a>
-                  </Menu.Item>}
+                  </Menu.Item>
+                )}
 
-                {user === 'cesfamCoordinators' &&
-                  <Menu.Item key="6" icon={<UsergroupAddOutlined style={{ color: '#51379c' }} />}>
+                {user === "cesfamCoordinators" && (
+                  <Menu.Item
+                    key="6"
+                    icon={<UsergroupAddOutlined style={{ color: "#51379c" }} />}
+                  >
                     <a href="/users-cesfam">Pacientes y Cuidadores</a>
-                  </Menu.Item>}
+                  </Menu.Item>
+                )}
 
-                {user === 'cesfamCoordinators' &&
-                  <Menu.Item key="7" icon={<UsergroupAddOutlined style={{ color: '#51379c' }} />}>
+                {user === "cesfamCoordinators" && (
+                  <Menu.Item
+                    key="7"
+                    icon={<UsergroupAddOutlined style={{ color: "#51379c" }} />}
+                  >
                     <a href="/register-requests">Solicitudes Registro</a>
-                  </Menu.Item>}
+                  </Menu.Item>
+                )}
 
-                {user === 'volunteers' &&
-                  <Menu.Item key="9" icon={<TeamOutlined style={{ color: '#51379c' }} />}>
+                {user === "volunteers" && (
+                  <Menu.Item
+                    key="9"
+                    icon={<TeamOutlined style={{ color: "#51379c" }} />}
+                  >
                     <span className="menuText">Pacientes</span>
                     <Link to="/patients-volunteers" />
-                  </Menu.Item>}
+                  </Menu.Item>
+                )}
 
-                {(user === 'patients' || user === 'caregivers') &&
-                  <Menu.Item key="11" icon={<PlusCircleOutlined style={{ color: '#51379c' }} />}>
+                {(user === "patients" || user === "caregivers") && (
+                  <Menu.Item
+                    key="11"
+                    icon={<PlusCircleOutlined style={{ color: "#51379c" }} />}
+                  >
                     <span className="menuText">Solicitud de ayuda</span>
                     <Link to="/form-help" />
-                  </Menu.Item>}
+                  </Menu.Item>
+                )}
 
-                {user === 'caregivers' &&
-                  <Menu.Item key="12" icon={<FormOutlined style={{ color: '#51379c' }} />}>
+                {user === "caregivers" && (
+                  <Menu.Item
+                    key="12"
+                    icon={<FormOutlined style={{ color: "#51379c" }} />}
+                  >
                     <span className="menuText">Reportes</span>
                     <Link to="/reports" />
-                  </Menu.Item>}
+                  </Menu.Item>
+                )}
 
-                <Menu.Item key="10" icon={<LogoutOutlined style={{ color: '#51379c' }} />}>
+                <Menu.Item
+                  key="10"
+                  icon={<LogoutOutlined style={{ color: "#51379c" }} />}
+                >
                   <Popconfirm
                     placement="bottomRight"
                     title={text}
@@ -196,8 +255,6 @@ function Principal(def) {
             </Sider>
 
             <Layout>
-
-
               <Card
                 style={{
                   minHeight: "800px",
@@ -208,20 +265,51 @@ function Principal(def) {
                 <Route exact path="/profile" component={Profile} />
                 <Route exact path="/help-board" component={HelpBoard} />
                 <Route path="/form-help" component={NewHelp} />
-                <Route exact path="/patients-volunteers" component={PatientsVolunteer} />
-                <Route exact path="/register-requests" component={RegisterRequests} />
-                <Route exact path="/volunteer-requests" component={VolunteerRequests} />
-                <Route exact path="/volunteers-coordinators" component={VolunteersCoordinator} />
-                <Route path="/volunteers-coordinators/new-ban/:id" component={NewBanVolunteer} />
+                <Route
+                  exact
+                  path="/patients-volunteers"
+                  component={PatientsVolunteer}
+                />
+                <Route
+                  exact
+                  path="/register-requests"
+                  component={RegisterRequests}
+                />
+                <Route
+                  exact
+                  path="/volunteer-requests"
+                  component={VolunteerRequests}
+                />
+                <Route
+                  exact
+                  path="/volunteers-coordinators"
+                  component={VolunteersCoordinator}
+                />
+                <Route
+                  path="/volunteers-coordinators/new-ban/:id"
+                  component={NewBanVolunteer}
+                />
                 <Route exact path="/activities" component={Activities} />
-                <Route path="/patients-volunteers/:id/new-activity" component={NewActivity} />
+                <Route
+                  path="/patients-volunteers/:id/new-activity"
+                  component={NewActivity}
+                />
                 <Route path="/activities/:id/report" component={Report} />
                 <Route exact path="/users-cesfam" component={UsersCesfam} />
-                <Route path="/users-cesfam/new-ban/:id" component={NewBanUser} />
-                <Route path="/activities/eval-activity/:id" component={EvalActivity} />
+                <Route
+                  path="/users-cesfam/new-ban/:id"
+                  component={NewBanUser}
+                />
+                <Route
+                  path="/activities/eval-activity/:id"
+                  component={EvalActivity}
+                />
                 <Route exact path="/reports" component={ReportsPatient} />
-                <Route exact path="/patients-coordinators" component={PatientsCoordinator} />
-
+                <Route
+                  exact
+                  path="/patients-coordinators"
+                  component={PatientsCoordinator}
+                />
               </Card>
             </Layout>
           </Layout>
